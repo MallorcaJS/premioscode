908491e2add67295fb9409cbc5e257aa38b18c79
Kevin O'Hagan <kevinohagan@gmail.com>
2015-04-02 12:34:35 +0200

    updated commnetsController

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('app', ['ionic', 'app.controllers', 'app.services','loginController','ngCordova','menu'])
+angular.module('app', ['ionic', 'app.controllers', 'app.services','ngCordova'])
 .constant("api", {
     "url": "http://localhost:8080",
     "static_url" : "images from client"
@@ -26,7 +26,7 @@ angular.module('app', ['ionic', 'app.controllers', 'app.services','loginControll
     .state('login', {
       url: '/login',
       templateUrl: 'templates/login.html',
-      controller: 'LoginCtrl'
+      controller: 'loginController'
     })
     .state('profile',{
       url : '/profile',
@@ -55,7 +55,7 @@ angular.module('app', ['ionic', 'app.controllers', 'app.services','loginControll
     .state('comments',{
       url:'/comments',
       templateUrl:'templates/comments.html',
-      controller: 'commentsCtrl'
+      controller: 'commentsController'
     })
 
   $urlRouterProvider.otherwise('/login');
--- app/www/js/commentsController.js
+++ app/www/js/commentsController.js
@@ -1,11 +1,13 @@
-angular.module('app.controllers').controller('commentsController', ['$scope', function ($scope) {
+angular.module('app.controllers').controller('commentsController', ['$scope', 'commentsService', function ($scope, commentsService) {
     
    $scope.shouldShowDelete = false;
    $scope.shouldShowReorder = false;
-   $scope.listCanSwipe = true
+   $scope.listCanSwipe = true;
    
     $scope.sayhi = function(){
         alert('super comments');
     }
+
+    $scope.comments = commentsService.getList();
 
 }]);
