00df4f4302eee2bd295ad0c4104d168f1d5c6304
Kevin O'Hagan <kevinohagan@gmail.com>
2015-04-02 11:53:18 +0200

    comments added to template

--- app/www/js/commentsController.js
+++ app/www/js/commentsController.js
@@ -1,7 +1,11 @@
 angular.module('app.controllers').controller('commentsController', ['$scope', function ($scope) {
     
+   $scope.shouldShowDelete = false;
+   $scope.shouldShowReorder = false;
+   $scope.listCanSwipe = true
+   
     $scope.sayhi = function(){
-        alert('hi');
+        alert('super comments');
     }
 
 }]);
