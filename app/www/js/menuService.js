7039f2f81811bbead4e0ed59859286ef150995ca
Jordi Roldán Llinàs <jordirolli@gmail.com>
2015-04-03 22:56:53 +0200

    · Agregar funcionalidad swipe con dos opciones para editar y eliminar entradas.

--- app/www/js/menuService.js
+++ app/www/js/menuService.js
@@ -4,17 +4,51 @@ angular.module('app.services').factory('menuService', ['$http', function ($http)
     getList: function(){
       return [
   			{
-  				"category" : "Salad",
+  				"category" : "Starter",
   				"options" :
   				[
             {
               "img": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ7Ap8AJT8KDpyLjVgBPyl7HowYywG0OhX53l7Evqa1ZBm4Gxok",
               "name": "Garden salad vinagrette dressing",
               "description": "bla bla bla"
+            },
+            {
+              "img": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTQ4L8kB3kT8ERAtleMOAB1c5vWjsgX1HY2NiKAjMrWH1aT2a5Oxg",
+              "name": "Coconut curry butternut squash sup",
+              "description": "bla bla bla"
+            }
+  				]
+  			},
+        {
+  				"category" : "Main",
+  				"options" :
+  				[
+            {
+              "img": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTnaOD-YkNT_u4xl4I5QTK4xTg4ajZWOiRnqACrwlw-elWoVemS",
+              "name": "Steak",
+              "description": "bla bla bla"
+            },
+            {
+              "img": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTC-1CU4_mTVZMfcdMRS3eZnRa4hB_0jzMAagvPUnwaGU1jKvmv",
+              "name": "Fish",
+              "description": "bla bla bla"
             }
   				]
   			}
   		];
+
+      /*
+      [
+            {
+              "img": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ7Ap8AJT8KDpyLjVgBPyl7HowYywG0OhX53l7Evqa1ZBm4Gxok",
+              "name": "Garden salad vinagrette dressing",
+              "description": "bla bla bla"
+            }
+          ];
+
+
+
+      */
     }
   }
 
