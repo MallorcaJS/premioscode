81c15e45528df14d219b7b0fac092fcc0d4aef7e
Xisco Lladó <xiscoo@gmail.com>
2015-04-06 09:37:17 +0200

    Done: 	- Login controller 	- added menú to all screens

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -5,33 +5,33 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('app', ['ionic', 'app.controllers', 'app.services', 'ngCordova'])
 .constant("api", {
-    "url": "http://localhost:8080",
-    "static_url" : "images from client"
+  "url": "http://localhost:8080",
+  "static_url" : "images from client"
 })
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+  if(window.cordova && window.cordova.plugins.Keyboard) {
+    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  }
+  if(window.StatusBar) {
+    StatusBar.styleDefault();
+  }
+});
 })
 .config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
-      .state('login', {
-      url: '/login',
-      templateUrl: 'templates/login.html',
-      controller: 'loginController'
-    })
     .state('layout',{
       url: '/layout',
       abstract: true,
       templateUrl: 'templates/layout.html'
+    })
+    .state('layout.login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'loginController'
     })
     .state('layout.profile',{
       url : '/profile',
@@ -62,6 +62,6 @@ angular.module('app', ['ionic', 'app.controllers', 'app.services', 'ngCordova'])
       controller: 'commentsController'
     });
 
-  $urlRouterProvider.otherwise('/login');
+    $urlRouterProvider.otherwise('/login');
 
-});
+  });
--- app/www/js/logincontroller.js
+++ app/www/js/logincontroller.js
@@ -1,7 +1,16 @@
 angular.module('app.controllers').controller('loginController', ['$scope','loginService', function ($scope, loginService) {
-    
-    $scope.sayhi = function(){
-        console.log(loginService.getLogin());
-    }
+
+	openFB.init({appId: '889540234418654'});
+
+	$scope.sayhi = function(){
+		openFB.login(
+		function(response) {
+			if(response.status === 'connected') {
+				alert('Facebook login succeeded, got access token: ' + response.authResponse.token);
+			} else {
+				alert('Facebook login failed: ' + response.error);
+			}
+		}, {scope: 'email,read_stream,publish_stream'});
+	}
 
 }]);
